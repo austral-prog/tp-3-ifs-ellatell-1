76cd20540cbb3d442fc6d950f54486b8e4715418
ellatell <etell@mail.austral.edu.ar>
2026-04-07 12:51:17 -0300

    Update exercise_age_check.py

--- exercise_age_check.py
+++ exercise_age_check.py
@@ -23,8 +23,9 @@ def age_check():
     limite_edad = int(input())
     if edad <= 0 or limite_edad <= 0:
         print("Entrada invalida")
-    elif edad > limite_edad: 
-        print("Eres mayor de edad") 
-    else: 
-        print("Eres menor de edad") 
+    elif edad >= limite_edad:
+        print("Eres mayor de edad")
+    else:
+        print("Eres menor de edad")
+
 
