de01db8ae9a5b2c0c789af7e448932ef198e2117
ellatell <etell@mail.austral.edu.ar>
2026-04-06 23:54:08 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -3,7 +3,7 @@ def grades():
     Ejercicio 5 - Clasificar Notas
 
     Leer una nota (0-10) mediante input(). Clasificar la nota e imprimir:
-    - "Excelente" si está entre 9 y 10
+    - "Excelente" si está entre 9 y 10 
     - "Bueno" si está entre 7 y 8
     - "Regular" si está entre 5 y 6
     - "Insuficiente" si está entre 0 y 4
@@ -18,4 +18,13 @@ def grades():
         Para la entrada "3", la salida esperada es:
         Insuficiente
     """
-    pass
+    pass 
+    nota = int(input())
+    if nota >= 9 and nota <= 10:
+        print("Excelente")
+    elif nota  >= 7 and nota <= 8:
+        print("Bueno")
+    elif nota >= 5 and nota <= 6:
+        print("Regular")
+    elif nota  >= 0 and  <= 4:
+        print("Insuficiente")
